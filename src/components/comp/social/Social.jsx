6d5a45a3a406c5eb2f9d7983a9119913279cdd99
Tarick <gabre.tarick@gmail.com>
2022-05-29 22:22:19 -0300

    portfolio in react and publish it to githubpages

--- src/components/comp/social/Social.jsx
+++ src/components/comp/social/Social.jsx
@@ -5,18 +5,21 @@ import { FaLinkedin } from 'react-icons/fa'
 function Social() {
     return (
         <div className="social-contact">
+
             <div className="social-icon-div">
+
                 <div className="social-icon-git">
                     <a href="https://github.com/p4n1k0">
-                        <FaGithub size={40} />
+                        <FaGithub size={50} />
                     </a>
                 </div>
 
                 <div className="social-icon-linkedin">
                     <a href="https://www.linkedin.com/in/gabriel-tarick-bb708917b/">
-                        <FaLinkedin size={40} />
+                        <FaLinkedin size={50} />
                     </a>
                 </div>
+
             </div>
         </div>
     )
