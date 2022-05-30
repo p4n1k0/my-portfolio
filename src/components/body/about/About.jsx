2b9cc9d2971ea9f6226503f3ee50e0655b1cbe92
Tarick <gabre.tarick@gmail.com>
2022-05-30 17:05:36 -0300

    cria funcao de habilidades e faz um map

--- src/components/body/about/About.jsx
+++ src/components/body/about/About.jsx
@@ -3,21 +3,23 @@ import './about.css';
 import logo from '../../../images/top.jpg';
 
 function About() {
-  const name = 'Gabriel Tarick A. Silva'
+  const name = <strong>Gabriel Tarick</strong>;
+
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
-          <p>Olá, Sou <br/>
-          <span className="about-name">{name}</span>.
-          <h6>A vida só gosta de quem gosta dela</h6>
-          Estudante de desenvolvimento web <br/>
-          Em várias linguagens de programação
-          </p>
+          <p>
+            Olá &#128075;, Sou<br/>
+            <span className="info-name">{name}</span>.<br />
+            <h6>Menos é mais. Ser simples requer tempo e esforço.</h6>
+            Estudante de desenvolvimento web <br />
+            Em várias linguagens de programação.
+          </p> 
         </div>
 
         <div className="about-photo">
-          <img src={logo} alt="gabriel-img" className="picture"/>
+          <img src={logo} alt="gabriel-img" className="picture" />
         </div>
 
       </div>
--- src/components/body/skill/Skills.jsx
+++ src/components/body/skill/Skills.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
+const Task = (value) => { return (<li key={value}>{value}</li>) };
+
+const habilidades = ['Git', 'HTML', 'CSS', 'JavasCript', 'DOM', 'React'];
+
 function Skills() {
   return (
     <div className="skills">
-      <div className="skills-info">
-        <ul>
-          <li>Git</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>DOM</li>
-          <li>React</li>
-        </ul>
+      <div className="skills-info">        
+        <ul>{habilidades.map((habilidade) => Task(habilidade))}</ul>
       </div>
     </div>
   )
