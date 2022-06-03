bfa92fb460d52adf8b8509c12d72531c4f105d1f
Tarick <gabre.tarick@gmail.com>
2022-06-02 22:59:13 -0300

    encapsula aplicacao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Home from './components/home/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (<Home />);
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,27 @@
+import React, { Component } from 'react';
 import Body from '../body/Body';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
 import "./home.css";
 
-function Home() {
-    return (
-        <div className="home">
-            <div>
-                <Header />
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <div>
+                    <Header />
+                </div>
+    
+                <div>
+                    <Body />
+                </div>
+    
+                <div>
+                    <Footer />
+                </div>
             </div>
-
-            <div>
-                <Body />
-            </div>
-
-            <div>
-                <Footer />
-            </div>
-        </div>
-    )
-
+        )
+    }    
 }
 
 export default Home;
